70d5dd71be4f535182a6df9763139642f9cdf11c
m.sharmsar@gmail.com <m.sharmsar@gmail.com>
2022-04-19 21:26:26 +0430

    Add reactnavigation6

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
